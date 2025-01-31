b06c56f34c38f59adc2601f585a1eca79b75ffc2
rts-Liva <U2451012@rts-365.dk>
2025-01-31 11:59:08 +0100

    temp loading

--- src/components/projects-list.jsx
+++ src/components/projects-list.jsx
@@ -5,18 +5,20 @@ function ProjectsList({ projects }) {
     return (
         <section className='projects-list'>
             <h1 className='projects-list__heading'>Mine projekter</h1>
-            <div className="projects">
-                {projects.length > 0 && projects.map((project, index) => (
-                    <div className="project" key={index}>
-                        <Link to={`/details/${project._id}`}><img src={project.path} alt="project poster" className="project__poster" /></Link>
-                        <div className="project__info">
-                            <Link to={`/details/${project._id}`}><h2 className="project__name">{project.name}</h2></Link>
-                            <p className='project__text'>{project.miniDescription}</p>
-                            <a href={project.github} target="_blank" className="project__link">Github repository</a>
+            {projects.length > 0 ? (
+                <div className="projects">
+                    {projects.length > 0 && projects.map((project, index) => (
+                        <div className="project" key={index}>
+                            <Link to={`/details/${project._id}`}><img src={project.path} alt="project poster" className="project__poster" /></Link>
+                            <div className="project__info">
+                                <Link to={`/details/${project._id}`}><h2 className="project__name">{project.name}</h2></Link>
+                                <p className='project__text'>{project.miniDescription}</p>
+                                <a href={project.github} target="_blank" className="project__link">Github repository</a>
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            ) : <p>Loading...</p>}
         </section>
     );
 }
